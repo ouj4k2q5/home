555c4ab92d63caa3f0071a123da8d11005ef76b4
ouj4k2q5 <ouj4k2q5@gmail.com>
2024-11-18 09:44:48 +0000

    expand icon size

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { LinkButton } from "@/components/ui/link-button";
-import { HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { HStack, Image, Link, Text, VStack } from "@chakra-ui/react";
 import { FaXTwitter, FaYoutube } from "react-icons/fa6";
 import "./App.css";
 
@@ -14,16 +13,27 @@ function App() {
       <Text fontSize="2xl" fontWeight="bold">
         ouj4k2q5
       </Text>
-      <HStack>
-        <LinkButton
+      <HStack gapX="20px">
+        <Link
           href="https://www.youtube.com/@ouj4k2q5"
           aria-label="go to YouTube"
+          _hover={{ color: "red.500", transform: "scale(1.1)" }}
+          _active={{ transform: "scale(0.9)" }}
+          _focus={{ color: "red.500", transform: "scale(1.1)" }}
+          transition="all 0.2s ease-in-out"
         >
-          <FaYoutube />
-        </LinkButton>
-        <LinkButton href="https://x.com/ouj4k2q5" aria-label="go to Twitter">
-          <FaXTwitter />
-        </LinkButton>
+          <FaYoutube size="35px" />
+        </Link>
+        <Link
+          href="https://x.com/ouj4k2q5"
+          aria-label="go to Twitter"
+          _hover={{ color: "blue.400", transform: "scale(1.1)" }}
+          _active={{ transform: "scale(0.9)" }}
+          _focus={{ color: "blue.400", transform: "scale(1.1)" }}
+          transition="all 0.2s ease-in-out"
+        >
+          <FaXTwitter size="30px" />
+        </Link>
       </HStack>
     </VStack>
   );
