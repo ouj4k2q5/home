53c74ab5c13adf5cbaac10b03cd648e2cdc9398c
ouj4k2q5 <ouj4k2q5@gmail.com>
2025-01-03 03:03:06 +0000

    Add title & GitHub icon on header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,10 +1,25 @@
-import { Box, Flex } from "@chakra-ui/react";
+import { Box, Flex, Link, Text } from "@chakra-ui/react";
+import { SiGithub } from "react-icons/si";
 
 export const Header = () => {
   return (
     <header>
-      <Box backgroundColor={"blackAlpha.300"}>
-        <Flex h={16}></Flex>
+      <Box backgroundColor={"blackAlpha.300"} px={4}>
+        <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+          <Text fontWeight="bold">ouj4k2q5</Text>
+          <Link
+            href="https://github.com/ouj4k2q5/home"
+            color="#e4e4e7"
+            aria-label="go to Github"
+            _hover={{ color: "gray.500", transform: "scale(1.1)" }}
+            _active={{ transform: "scale(0.9)" }}
+            _focus={{ color: "gray.500", transform: "scale(1.1)" }}
+            transition="all 0.5s ease-in-out"
+            focusRingColor="blackAlpha.200"
+          >
+            <SiGithub size="20px" />
+          </Link>
+        </Flex>
       </Box>
     </header>
   );
