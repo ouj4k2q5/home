25537b383b6552a5126df0bb747d89f887f46b07
ouj4k2q5 <ouj4k2q5@gmail.com>
2025-04-29 00:46:13 +0000

    Add link to header icon

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,9 +6,27 @@ export const Header = () => {
     <header>
       <Box backgroundColor={"blackAlpha.300"} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-          <Text fontWeight="bold" color="#e4e4e7">
-            ouj4k2q5
-          </Text>
+          <Link
+            variant="plain"
+            href="https://ouj4k2q5.f5.si/"
+            aria-label="go to top page"
+            _hover={{
+              color: "gray.500",
+              transform: "scale(1.1)",
+              textDecoration: "none",
+            }}
+            _active={{ transform: "scale(0.9)" }}
+            _focus={{
+              color: "gray.500",
+              transform: "scale(1.1)",
+              textDecoration: "none",
+            }}
+            transition="all 0.5s ease-in-out"
+          >
+            <Text fontWeight="bold" color="#e4e4e7">
+              ouj4k2q5
+            </Text>
+          </Link>
           <Link
             href="https://github.com/ouj4k2q5/home"
             color="#e4e4e7"
